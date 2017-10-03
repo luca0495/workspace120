2c487e95cbf42aaf27b79d00fb0b9822587e8e8d
DEXO16\Mauro <dexa@hotmail.it>
2017-10-03 17:34:48 +0200

    modifica clients

--- Schoolib/src/Core/Clients.java
+++ Schoolib/src/Core/Clients.java
@@ -1,6 +1,8 @@
 package Core;
 
 import java.io.Serializable;
+// prova lavatrice avete seguito il discorso
+// no
 
 public enum Clients implements Serializable {
 Admin,Librarian,Reader,Guest,Default
