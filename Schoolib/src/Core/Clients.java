f334c62c2a2d050a72287aa52e5393fe070c5069
DEXO16\Mauro <dexa@hotmail.it>
2017-10-03 17:46:46 +0200

    modifica commento client 2 versione

--- Schoolib/src/Core/Clients.java
+++ Schoolib/src/Core/Clients.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 // prova lavatrice avete seguito il discorso
 // no
 // no no no
+// oh no , no , no , no...
 
 public enum Clients implements Serializable {
 Admin,Librarian,Reader,Guest,Default
